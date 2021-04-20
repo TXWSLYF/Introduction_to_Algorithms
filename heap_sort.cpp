c5f4f0d2e6cc7fd58bd4adf7a8b148b136b6736d
alex <1060319328@qq.com>
2021-04-20 23:39:59 +0800

    feat: heap_sort

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -2,19 +2,17 @@
 
 using namespace std;
 
+// 维持最大堆
 void max_heapify(int a[], int i, int heap_size)
 {
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    int largest;
+    int largest = i;
+    int left = 2 * largest + 1;
+    int right = 2 * largest + 2;
 
+    // 防止越界
     if (left < heap_size && a[left] > a[i])
     {
         largest = left;
-    }
-    else
-    {
-        largest = i;
     }
 
     if (right < heap_size && a[right] > a[largest])
@@ -28,13 +26,66 @@ void max_heapify(int a[], int i, int heap_size)
         a[i] = a[largest];
         a[largest] = temp;
 
-        max_heapify(a, largest);
+        max_heapify(a, largest, heap_size);
     }
 }
 
+// 维持最大堆，非递归版
+void max_heapify_2(int a[], int i, int heap_size)
+{
+    while (true)
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < heap_size && a[left] > a[i])
+        {
+            largest = left;
+        }
+
+        if (right < heap_size && a[right] > a[largest])
+        {
+            largest = right;
+        }
+
+        if (largest == i)
+            return;
+
+        int temp = a[i];
+        a[i] = a[largest];
+        a[largest] = temp;
+
+        i = largest;
+    }
+}
+
+// 构建最大堆
+void build_max_heap(int a[], int l)
+{
+    for (int i = l / 2 - 1; i >= 0; i--)
+    {
+        max_heapify(a, i, l);
+    }
+}
+
+// 堆排序
 void heap_sort(int a[], int l)
 {
-    max_heapify(a, 0, l);
+    int heap_size = l;
+
+    build_max_heap(a, heap_size);
+
+    for (int i = heap_size - 1; i > 0; i--)
+    {
+        int temp = a[0];
+        a[0] = a[i];
+        a[i] = temp;
+
+        --heap_size;
+
+        max_heapify(a, 0, heap_size);
+    }
 }
 
 int main()
@@ -42,7 +93,7 @@ int main()
     int a[10] = {16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
     int heap_size = 10;
 
-    heap_sort(a, 10);
+    heap_sort(a, heap_size);
 
     for (auto i : a)
     {
